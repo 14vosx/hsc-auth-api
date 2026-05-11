69698e9dbe473fab8ad30853e4be17e8224f1e62
14vosx <14vosx@gmail.com>
2026-05-11 16:06:34 -0300

    feat(player-bunker): add season player artifact loader (#73)

---
+++ src/config/playerBunker.js
@@ -0,0 +1,7 @@
+// src/config/playerBunker.js
+
+export const PLAYER_BUNKER_ARTIFACT_ROOT =
+  process.env.PLAYER_BUNKER_ARTIFACT_ROOT || "";
+
+export const PLAYER_BUNKER_ACTIVE_SEASON_SLUG =
+  process.env.PLAYER_BUNKER_ACTIVE_SEASON_SLUG || "";
